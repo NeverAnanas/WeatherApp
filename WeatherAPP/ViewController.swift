560fc7ecf6fb07cc8c3d7d71629f6cc8dd2ca37d
Анастасия <neverova1994nasty@gmail.com>
2024-05-28 10:46:51 +0300

    Add some layout

--- WeatherAPP/ViewController.swift
+++ WeatherAPP/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
@@ -20,52 +21,114 @@ class ViewController: UIViewController {
     private let perception: String = "ощущается как"
     private let circle: String = "°"
     
+    let topContainer = UIImageView()
+    let currentDayLabel = UILabel()
+    let currentWeatherIconView = UIImageView()
+    let currentTemperatureLabel = UILabel()
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let labelCity = UILabel(
-            frame: .init(
-                x: 50,
-                y: 70,
-                width: 300, height: 90
-            )
-        )
+        layoutCurrentWeatherView()
         
-        let font = UIFont.boldSystemFont(ofSize: 27)
-
-        let labelToDay = UILabel(
-            frame: .init(
-                x: 70,
-                y: 90,
-                width: 300, height: 90)
-            )
+//        let labelCity = UILabel(
+//            frame: .init(
+//                x: 50,
+//                y: 70,
+//                width: 300, height: 90
+//            )
+//        )
+//        
+//        let font = UIFont.boldSystemFont(ofSize: 27)
+//
+//        let labelToDay = UILabel(
+//            frame: .init(
+//                x: 70,
+//                y: 90,
+//                width: 300, height: 90)
+//            )
+//        
+//        var backGraundWeather = UIImageView()
+//        var myImage = UIImageView()
+//        
+//        let backGraundWeatherBlue = UIImage(named: "Group 33510")
+//        let backGraundWeatherSun = UIImage(named: "Sun")
+//        
+//        backGraundWeather = UIImageView(image: backGraundWeatherBlue)
+//        myImage = UIImageView(image: backGraundWeatherSun)
+//        
+//        backGraundWeather.frame = CGRect(x: 25, y: 170, width: 343, height: 280)
+//        myImage.frame = CGRect(x: 25, y: 170, width: 343, height: 280)
+//        
+//        
+//        labelCity.text = city
+//        labelCity.font = font
+//        labelCity.textAlignment = .center
+//        labelToDay.text = toDay
+//        
+//        view.addSubview(labelCity)
+//        view.addSubview(labelToDay)
+//        view.addSubview(backGraundWeather)
+//        view.addSubview(myImage)
         
-        var backGraundWeather = UIImageView()
-        var myImage = UIImageView()
+    }
+    
+    func layoutCurrentWeatherView() {
+        // background image
+        view.addSubview(topContainer)
+        topContainer.translatesAutoresizingMaskIntoConstraints = false
         
-        let backGraundWeatherBlue = UIImage(named: "Group 33510")
-        let backGraundWeatherSun = UIImage(named: "Sun")
+        topContainer.image = UIImage(named: "Group 33510")
+        topContainer.contentMode = .scaleAspectFit
         
-        backGraundWeather = UIImageView(image: backGraundWeatherBlue)
-        myImage = UIImageView(image: backGraundWeatherSun)
+        NSLayoutConstraint.activate([
+            topContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            topContainer.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            topContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            topContainer.heightAnchor.constraint(equalToConstant: 280)
+        ])
         
-        backGraundWeather.frame = CGRect(x: 25, y: 170, width: 343, height: 280)
-        myImage.frame = CGRect(x: 25, y: 170, width: 343, height: 280)
+        // text
+        topContainer.addSubview(currentDayLabel)
         
+        currentDayLabel.translatesAutoresizingMaskIntoConstraints = false
+        currentDayLabel.text = "12 May"
+        currentDayLabel.font = .systemFont(ofSize: 14, weight: .semibold)
+        currentDayLabel.textColor = .white
+        currentDayLabel.textAlignment = .center
         
+        NSLayoutConstraint.activate([
+            currentDayLabel.topAnchor.constraint(equalTo: topContainer.topAnchor, constant: 16),
+            currentDayLabel.centerXAnchor.constraint(equalTo: topContainer.centerXAnchor),
+            currentDayLabel.leadingAnchor.constraint(greaterThanOrEqualTo: topContainer.leadingAnchor, constant: 50),
+            currentDayLabel.trailingAnchor.constraint(greaterThanOrEqualTo: topContainer.trailingAnchor, constant: -50),
+        ])
         
-        labelCity.text = city
-        labelCity.font = font
-        labelCity.textAlignment = .center
-        labelToDay.text = toDay
+        // image
+        topContainer.addSubview(currentWeatherIconView)
+        currentWeatherIconView.translatesAutoresizingMaskIntoConstraints = false
         
-        view.addSubview(labelCity)
-        view.addSubview(labelToDay)
-        view.addSubview(backGraundWeather)
-        view.addSubview(myImage)
+        currentWeatherIconView.image = UIImage(named: "Sun")
+        currentWeatherIconView.backgroundColor = .red
         
+        NSLayoutConstraint.activate([
+            currentWeatherIconView.topAnchor.constraint(equalTo: currentDayLabel.bottomAnchor, constant: 12),
+            currentWeatherIconView.centerXAnchor.constraint(equalTo: currentDayLabel.centerXAnchor)
+        ])
+        
+        // temperature label
+        
+        topContainer.addSubview(currentTemperatureLabel)
+        currentTemperatureLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        currentTemperatureLabel.text = "30°"
+        currentTemperatureLabel.font = .systemFont(ofSize: 40, weight: .bold)
+        currentTemperatureLabel.textColor = .white
+        
+        NSLayoutConstraint.activate([
+            currentTemperatureLabel.topAnchor.constraint(equalTo: currentWeatherIconView.bottomAnchor, constant: 10),
+            currentTemperatureLabel.centerXAnchor.constraint(equalTo: topContainer.centerXAnchor)
+        ])
     }
 }
 
