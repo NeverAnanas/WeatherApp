f9b4d84a9bf7d13ce41d704e1e5caf804b025889
Анастасия <neverova1994nasty@gmail.com>
2024-06-06 12:04:31 +0300

    delet trash and code migration

--- WeatherAPP/MainScreen/MainScreenViewController.swift
+++ WeatherAPP/MainScreen/MainScreenViewController.swift
@@ -8,41 +8,66 @@
 
 import UIKit
 
-struct WeatherItem {
-    let time: String
-    let temperature: String
-    let emoji: String
-}
-
 class MainScreenViewController: UIViewController  {
     let weatherCellGray = WeatherCellGray()
     let weatherCellGray2 = WeatherCellGray()
+    let weatherCellGray3 = WeatherCellGray()
+    let weatherCellGray4 = WeatherCellGray()
     let scrollView = UIScrollView()
     let topContainer = UIImageView()
     let currentDayLabel = UILabel()
     let currentWeatherIconView = UIImageView()
     let currentTemperatureLabel = UILabel()
     let currentFeelTemperatureLabel = UILabel()
-
     
-
+    lazy var day1 = WeatherCellGray.WeatherDay(
+        date: "13 августа",
+        temperature: "25°",
+        feelTemperature: "29°",
+        dayOfWeek: "пн",
+        eachHourForecast: weatherItemsForMonday
+    )
+    
+    lazy var day2 = WeatherCellGray.WeatherDay(
+        date: "14 августа",
+        temperature: "25°",
+        feelTemperature: "26°",
+        dayOfWeek: "пн",
+        eachHourForecast: weatherItemsForMonday
+    )
+    
+    lazy var day3 = WeatherCellGray.WeatherDay(
+        date: "15 августа",
+        temperature: "23°",
+        feelTemperature: "20°",
+        dayOfWeek: "пн",
+        eachHourForecast: weatherItemsForMonday
+    )
+    
+    lazy var day4 = WeatherCellGray.WeatherDay(
+        date: "16 августа",
+        temperature: "25°",
+        feelTemperature: "20°",
+        dayOfWeek: "пн",
+        eachHourForecast: weatherItemsForMonday
+    )
 
     let weatherItemsForMonday = [
-        WeatherItem(time: "12:00", temperature: "25°", emoji: "mini_sun_icon"),
-        WeatherItem(time: "15:00", temperature: "25°", emoji: "mini_sun_icon"),
-        WeatherItem(time: "18:00", temperature: "25°", emoji: "mini_sun_icon"),
-        WeatherItem(time: "21:00", temperature: "21°", emoji: "mini_sun_icon"),
-        WeatherItem(time: "01:00", temperature: "20°", emoji: "mini_sun_icon"),
-        WeatherItem(time: "04:00", temperature: "18°", emoji: "mini_sun_icon")
+        WeatherItemCell.WeatherItem(time: "12:00", temperature: "25°", emoji: "mini_sun_icon"),
+        WeatherItemCell.WeatherItem(time: "15:00", temperature: "25°", emoji: "mini_sun_icon"),
+        WeatherItemCell.WeatherItem(time: "18:00", temperature: "25°", emoji: "mini_sun_icon"),
+        WeatherItemCell.WeatherItem(time: "21:00", temperature: "21°", emoji: "mini_sun_icon"),
+        WeatherItemCell.WeatherItem(time: "01:00", temperature: "20°", emoji: "mini_sun_icon"),
+        WeatherItemCell.WeatherItem(time: "04:00", temperature: "18°", emoji: "mini_sun_icon")
     ]
     
     let weatherItemsForSunday = [
-        WeatherItem(time: "12:00", temperature: "25°", emoji: "mini_sun_icon"),
-        WeatherItem(time: "15:00", temperature: "25°", emoji: "mini_sun_icon"),
-        WeatherItem(time: "18:00", temperature: "25°", emoji: "mini_sun_icon"),
-        WeatherItem(time: "21:00", temperature: "21°", emoji: "mini_sun_icon"),
-        WeatherItem(time: "01:00", temperature: "20°", emoji: "mini_sun_icon"),
-        WeatherItem(time: "04:00", temperature: "18°", emoji: "mini_sun_icon")
+        WeatherItemCell.WeatherItem(time: "12:00", temperature: "25°", emoji: "mini_sun_icon"),
+        WeatherItemCell.WeatherItem(time: "15:00", temperature: "25°", emoji: "mini_sun_icon"),
+        WeatherItemCell.WeatherItem(time: "18:00", temperature: "25°", emoji: "mini_sun_icon"),
+        WeatherItemCell.WeatherItem(time: "21:00", temperature: "21°", emoji: "mini_sun_icon"),
+        WeatherItemCell.WeatherItem(time: "01:00", temperature: "20°", emoji: "mini_sun_icon"),
+        WeatherItemCell.WeatherItem(time: "04:00", temperature: "18°", emoji: "mini_sun_icon")
     ]
     
     let item = UIBarButtonItem(systemItem: .close)
@@ -52,15 +77,14 @@ class MainScreenViewController: UIViewController  {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        weatherCellGray.setWeather(weatherItemsForMonday)
-        weatherCellGray2.setWeather(weatherItemsForSunday)
+    
+        weatherCellGray.setWeather(day: day1)
+        weatherCellGray2.setWeather(day: day2)
+        weatherCellGray3.setWeather(day: day3)
+        weatherCellGray4.setWeather(day: day4)
         
         topContainer.isUserInteractionEnabled = true
-        
-        
-        
+
         // navigation Item title (Нижний Новгород)
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(buttonTappedAction))
@@ -126,11 +150,12 @@ class MainScreenViewController: UIViewController  {
         topContainer.translatesAutoresizingMaskIntoConstraints = false
         
         topContainer.image = UIImage(named: "Group 33510")
-        topContainer.contentMode = .scaleAspectFit
+        topContainer.contentMode = .scaleAspectFill
         
         NSLayoutConstraint.activate([
             topContainer.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
             topContainer.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 10),
+            topContainer.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
             topContainer.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
             topContainer.heightAnchor.constraint(equalToConstant: 280)
         ])
@@ -200,11 +225,44 @@ class MainScreenViewController: UIViewController  {
         NSLayoutConstraint.activate([
             weatherCellGray.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
             weatherCellGray.topAnchor.constraint(equalTo: topContainer.bottomAnchor, constant: 20),
+            weatherCellGray.centerXAnchor.constraint(equalTo: topContainer.centerXAnchor),
             weatherCellGray.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
             weatherCellGray.heightAnchor.constraint(equalToConstant: 206)
         ])
         
         scrollView.addSubview(weatherCellGray2)
+        weatherCellGray2.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            weatherCellGray2.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
+            weatherCellGray2.topAnchor.constraint(equalTo: weatherCellGray.bottomAnchor, constant: 8),
+            weatherCellGray2.centerXAnchor.constraint(equalTo: topContainer.centerXAnchor),
+            weatherCellGray2.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
+            weatherCellGray2.heightAnchor.constraint(equalToConstant: 206)
+        ])
+        
+        scrollView.addSubview(weatherCellGray3)
+        weatherCellGray3.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            weatherCellGray3.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
+            weatherCellGray3.topAnchor.constraint(equalTo: weatherCellGray2.bottomAnchor, constant: 8),
+            weatherCellGray3.centerXAnchor.constraint(equalTo: topContainer.centerXAnchor),
+            weatherCellGray3.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
+            weatherCellGray3.heightAnchor.constraint(equalToConstant: 206)
+        ])
+        
+        scrollView.addSubview(weatherCellGray4)
+        weatherCellGray4.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            weatherCellGray4.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
+            weatherCellGray4.topAnchor.constraint(equalTo: weatherCellGray3.bottomAnchor, constant: 8),
+            weatherCellGray4.centerXAnchor.constraint(equalTo: topContainer.centerXAnchor),
+            weatherCellGray4.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
+            weatherCellGray4.heightAnchor.constraint(equalToConstant: 206),
+            weatherCellGray4.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 10)
+        ])
     }
 }
 
