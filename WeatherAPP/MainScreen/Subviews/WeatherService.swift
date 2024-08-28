0a8097d00df4b937b9821acc2cd36eb2b656434b
Анастасия <neverova1994nasty@gmail.com>
2024-08-28 13:29:18 +0300

    refactoring

--- WeatherAPP/MainScreen/Subviews/WeatherService.swift
+++ WeatherAPP/MainScreen/Subviews/WeatherService.swift
@@ -1,9 +1,49 @@
 import UIKit
 
-struct WeatherData: Codable {
+struct Forecast: Codable {
+    let date: Date
     let temperature: Double
     let description: String
+    let feelsLike: Double
+    let main: String
+    let icon: String
+    let id: Int
 }
+
+struct WeatherResponse: Codable {
+    let list: [APIForecast]
+}
+
+struct APIForecast: Codable {
+    let dt: Double
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+    let feelsLike: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+    }
+}
+
+struct Weather: Codable {
+    let textDescription: String
+    let id: Int        // Идникатор погодных условий
+    let main: String   // Группа параметров погоды (дождь, снег, облака и т. д.)
+    let icon: String   // Идентор значка погоды
+    
+    enum CodingKeys: String, CodingKey {
+        case textDescription = "description"
+        case id
+        case main
+        case icon
+    }
+}
+
 
 class WeatherService {
     let apiKey = "800ca88bde916f0238757457c4e63bda"
@@ -11,9 +51,8 @@ class WeatherService {
     let lon = 44
 
     
-    func fetchWeather(for city: String, completion: @escaping (WeatherData?) -> Void) {
+    func fetchWeather(for city: String, completion: @escaping ([Forecast]?) -> Void) {
         let baseURL = "https://api.openweathermap.org/data/2.5/forecast?lat=\(lat)&lon=\(lon)&appid=\(apiKey)&units=metric"
-//        let urlString = "\(baseURL)?q=\(city)&appid=\(apiKey)&units=metric"
         
         guard let url = URL(string: baseURL) else {
             completion(nil)
@@ -25,35 +64,39 @@ class WeatherService {
                 let decoder = JSONDecoder()
                 do {
                     let weatherResponse = try decoder.decode(WeatherResponse.self, from: data)
-                    let weatherData = WeatherData(
-                        temperature: weatherResponse.list.first?.main.temp ?? 0.0,
-                        description: weatherResponse.list.first?.weather.description ?? ""
-                    )
-                    completion(weatherData)
+                    
+                    let forecasts = weatherResponse.list.map { forecastDay in
+                        Forecast(
+                            date: self.createDate(from: forecastDay.dt),
+                            temperature: forecastDay.main.temp,
+                            description: forecastDay.main.temp.description,
+                            feelsLike: forecastDay.main.feelsLike,
+                            main: forecastDay.weather.first?.main ?? "",
+                            icon: forecastDay.weather.first?.icon ?? "",
+                            id: forecastDay.weather.first?.id ?? 0
+                        )
+                    }
+                    
+                    completion(forecasts)
                 } catch {
+//                    assertionFailure()
                     completion(nil)
                 }
             } else {
+//                assertionFailure()
                 completion(nil)
             }
         }
+        
         task.resume()
     }
-}
-
-struct WeatherResponse: Codable {
-    let list: [ForecastDay]
-}
-
-struct ForecastDay: Codable {
-    let main: Main
-    let weather: [Weather]
-}
-
-struct Main: Codable {
-    let temp: Double
-}
-
-struct Weather: Codable {
-    let description: String
+    
+    private func createDate(from unixRepresentation: Double) -> Date {
+        var date = Date(timeIntervalSince1970: unixRepresentation)
+        let secondsIn3Hours = 60 * 60 * 3
+        let timeOffsetForGMT3 = TimeInterval(exactly: secondsIn3Hours)!
+        
+        date.addTimeInterval(timeOffsetForGMT3)
+        return date
+    }
 }
---
+++ WeatherData.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherData.swift
+//  WeatherAPP
+//
+//  Created by Анастасия Неверова on 28.08.2024.
+//
+
+import Foundation
