8f71558333b1cef2f3c6527a26a1964f29199638
Анастасия <neverova1994nasty@gmail.com>
2024-10-07 11:10:25 +0300

    create image resolver

---
+++ WeatherAPP/MainScreen/Subviews/ImageResolver.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+protocol IImageResolver {
+    func resolve(completion: @escaping (UIImage?) -> Void)
+}
+
+class ImageResolver: IImageResolver {
+    private let imageName: String
+    
+    init(imageName: String) {
+        self.imageName = imageName
+    }
+    
+    func resolve(completion: @escaping (UIImage?) -> Void) {
+        let iconURL = "https://openweathermap.org/img/wn/\(imageName)@2x.png"
+        
+        guard let url = URL(string: iconURL) else {
+            completion(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let data = data {
+                let icon = UIImage(data: data)
+                completion(icon)
+            } else {
+                completion(nil)
+            }
+        }
+        task.resume()
+    }
+        
+}
+    
+
